a0da25b2b75aa306775621d3fb6e9193e6fceb7c
Lior12321 <lior.m.masas@gmail.com>
2024-05-13 13:08:14 +0300

    Initial commit

---
+++ src/primitives/Point.java
@@ -0,0 +1,99 @@
+package primitives;
+
+/**
+ * The Point class represents a point in three-dimensional space.
+ */
+public class Point {
+    protected Double3 xyz;
+
+    /**
+     * Constructs a new Point with the specified coordinates.
+     * @param xyz The coordinates of the point.
+     */
+    public Point(Double3 xyz) {
+        this.xyz = xyz;
+    }
+
+    /**
+     * Constructs a new Point with the specified coordinates.
+     * @param x The x-coordinate of the point.
+     * @param y The y-coordinate of the point.
+     * @param z The z-coordinate of the point.
+     */
+    public Point(double x, double y, double z) {
+        super();
+    }
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * @param obj The reference object with which to compare.
+     * @return true if this object is the same as the obj argument; false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        return (obj instanceof Point other) && this.xyz.equals(other.xyz);
+    }
+
+    /**
+     * Returns a string representation of this Point.
+     * @return A string representation of this Point.
+     */
+    @Override
+    public String toString() {
+        return "(" + xyz.d1 + "," + xyz.d2 + "," + xyz.d3 + ")";
+    }
+
+    /**
+     * Adds a vector to this point and returns the result as a new point.
+     * @param vec The vector to add.
+     * @return A new point which is the result of adding the vector to this point.
+     */
+    public Point add(primitives.Vector vec) {
+        double x, y, z;
+        x = xyz.d1 + vec.xyz.d1;
+        y = xyz.d2 + vec.xyz.d2;
+        z = xyz.d3 + vec.xyz.d3;
+        Double3 newD = new Double3(x, y, z);
+        Point newP = new Point(newD);
+        return newP;
+    }
+
+    /**
+     * Subtracts another point from this point and returns the result as a new vector.
+     * @param P The point to subtract.
+     * @return A new vector which is the result of subtracting the given point from this point.
+     */
+    public primitives.Vector subtract(Point P) {
+        double x, y, z;
+        x = xyz.d1 - P.xyz.d1;
+        y = xyz.d2 - P.xyz.d2;
+        z = xyz.d3 - P.xyz.d3;
+        Double3 newV = new Double3(x, y, z);
+        primitives.Vector vec = new primitives.Vector(newV);
+        return vec;
+    }
+
+    /**
+     * Computes the squared distance between this point and another point.
+     * @param P The other point.
+     * @return The squared distance between this point and the other point.
+     */
+    public double distanceSquared(Point P) {
+        double x, y, z;
+        x = (xyz.d1 - P.xyz.d1) * (xyz.d1 - P.xyz.d1);
+        y = (xyz.d2 - P.xyz.d2) * (xyz.d2 - P.xyz.d2);
+        z = (xyz.d3 - P.xyz.d3) * (xyz.d3 - P.xyz.d3);
+        return x + y + z;
+    }
+
+    /**
+     * Computes the distance between this point and another point.
+     * @param P The other point.
+     * @return The distance between this point and the other point.
+     */
+    public double distance(Point P) {
+        return Math.sqrt(this.distanceSquared(P));
+    }
+}
---
+++ src/primitives/Vector.java
@@ -0,0 +1,122 @@
+package primitives;
+
+public class Vector extends Point {
+
+	/**
+	 * Constructs a new Vector with the specified coordinates.
+	 *
+	 * @param x The x-coordinate of the vector.
+	 * @param y The y-coordinate of the vector.
+	 * @param z The z-coordinate of the vector.
+	 * @throws IllegalArgumentException if the input vector is a zero vector.
+	 */
+	public Vector(Double x, double y, double z) {
+		super(x, y, z);
+		Double3 newD = new Double3(x, y, z);
+		if (newD.equals(Double3.ZERO))
+			throw new IllegalArgumentException("You enterd a zero vector!");
+
+	}
+
+	/**
+	 * Constructs a new Vector from a Double3 object.
+	 *
+	 * @param newD The Double3 object representing the vector.
+	 * @throws IllegalArgumentException if the input vector is a zero vector.
+	 */
+	public Vector(Double3 newD) {
+		super(newD);
+		if (newD.equals(Double3.ZERO))
+			throw new IllegalArgumentException("You enterd a zero vector!");
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return false;
+		// super();
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+
+	/**
+	 * Calculates the squared length of this vector.
+	 * 
+	 * @return The squared length of this vector.
+	 */
+	public double lengthSquared() {
+		return (xyz.d1 * xyz.d1 + xyz.d2 * xyz.d2 + xyz.d3 * xyz.d3);
+	}
+
+	/**
+	 * Calculates the length of this vector.
+	 * 
+	 * @return The length of this vector.
+	 */
+	public double length() {
+		return Math.sqrt(this.lengthSquared());
+	}
+
+	/**
+	 * Adds another vector to this vector and returns the result as a new vector.
+	 * 
+	 * @param vec The vector to be added to this vector.
+	 * @return A new vector which is the result of adding the given vector to this
+	 *         vector.
+	 */
+	public Vector add(Vector vec) {
+		Vector newVec = new Vector(xyz.d1 + vec.xyz.d1, xyz.d2 + vec.xyz.d2, xyz.d3 + vec.xyz.d3);
+		return newVec;
+	}
+
+	/**
+	 * multiply this vector by a scalar factor.
+	 * 
+	 * @param num The scalar factor by which to scale the vector.
+	 * @return A new vector which is the result of scaling this vector by the given
+	 *         factor.
+	 */
+	public Vector scale(double num) {
+		Vector newVec = new Vector(xyz.d1 * num, xyz.d2 * num, xyz.d3 * num);
+		return newVec;
+	}
+
+	/**
+	 * Calculate the dot product of this vector with another vector.
+	 * 
+	 * @param vec The vector to calculate the dot product with.
+	 * @return A new vector which is the result of the dot product operation.
+	 */
+	public Vector dotProduct(Vector vec) {
+		Double3 newD = new Double3(xyz.d1 * vec.xyz.d1, xyz.d2 * vec.xyz.d2, xyz.d3 * vec.xyz.d3);
+		Vector newV = new Vector(newD);
+		return newV;
+	}
+
+	/**
+	 * Calculate the cross product of this vector with another vector.
+	 * 
+	 * @param vec The vector to compute the cross product with.
+	 * @return A new vector which is the result of the cross product operation.
+	 */
+	public Vector crossProduct(Vector vec) {
+		double x, y, z;
+		x = xyz.d2 * vec.xyz.d3 - xyz.d3 * vec.xyz.d2;
+		y = xyz.d3 * vec.xyz.d1 - xyz.d1 * vec.xyz.d3;
+		z = xyz.d1 * vec.xyz.d2 - xyz.d2 * vec.xyz.d1;
+		Vector newVec = new Vector(x, y, z);
+		return newVec;
+	}
+
+	/**
+	 * Returns a new vector that is the normalized form of this vector.
+	 * @return A new vector that has the same direction as this vector but with a length of 1.
+	 */
+	public Vector normalize() {
+		double size = this.lengthSquared();
+		Vector newVec = new Vector(xyz.d1 / size, xyz.d2 / size, xyz.d3 / size);
+		return newVec;
+	}
+}
