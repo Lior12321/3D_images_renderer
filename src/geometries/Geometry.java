8ba1e8cc7cfb7557faaafcdb7fcf12c8cf414006
Lior12321 <lior.m.masas@gmail.com>
2024-05-19 21:52:16 +0300

    geo + radial

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,12 +1,21 @@
 package geometries;
-
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * The Geometry interface represents geometric shapes in a 3D space. Any class
+ * that implements this interface should be able to provide a normal vector to
+ * the shape at given points.
+ * 
+ * @author Lior &amp; Asaf
+ */
 public interface Geometry {
-	
-	public Vector getNormal(Point... p);
-
-	//public Polygon(Point... vertices) {
-	//Vector getNormal();
+    
+    /**
+     * Returns the normal vector to the geometry at the specified points.
+     * 
+     * @param p one or zero points on the geometry where the normal is to be calculated
+     * @return the normal vector at the specified points
+     */
+    public Vector getNormal(Point... p);
 }
--- src/geometries/RadialGeometry.java
+++ src/geometries/RadialGeometry.java
@@ -1,10 +1,25 @@
 package geometries;
 
+/**
+ * The RadialGeometry class represents a radial geometric shape characterized by
+ * a radius. It is an abstract class that serves as a base for all radial
+ * geometries.
+ * 
+ * @author Lior &amp; Asaf
+ */
 public abstract class RadialGeometry implements Geometry {
 	private final double radius;
 
+	/**
+	 * Constructs a RadialGeometry with the specified radius.
+	 *
+	 * @param radius the radius of the radial geometry. It must be a positive
+	 *               number.
+	 * @throws IllegalArgumentException if the radius is not a positive number.
+	 */
 	public RadialGeometry(double radius) {
+		if (radius <= 0)
+			throw new IllegalArgumentException("The radius must be positive number!");
 		this.radius = radius;
-			
 	}
 }
