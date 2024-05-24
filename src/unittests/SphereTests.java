034574448b7b4057aa1528c9319a39697dc967af
asafyhv <66057736+asafyhv@users.noreply.github.com>
2024-05-24 13:48:33 +0300

    Sphere tests

--- src/unittests/SphereTests.java
+++ src/unittests/SphereTests.java
@@ -16,21 +16,15 @@ import org.junit.jupiter.api.Test;
  * 
  */
 class SphereTests {
-
-	/**
-	 * Test method for {@link geometries.Sphere#getNormal(primitives.Point[])}.
-	 */
-	@Test
-	void testGetNormalPointArray() {
-		fail("Not yet implemented");
-	}
-
 	/**
 	 * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
 	 */
 	@Test
 	void testGetNormalPoint() {
-		fail("Not yet implemented");
+		Sphere s1 = new Sphere(new Point(1, 2, 3), 1);
+		// ============ Equivalence Partitions Tests ==============
+		Vector v1 = s1.getNormal(new Point(2, 2, 3));
+		// TC01: Test that the normal is the correct one
+		assertEquals(new Vector(1, 0, 0), v1, "getNormal() wrong result");
 	}
-
 }
