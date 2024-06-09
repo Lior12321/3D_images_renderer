9a1a3971b5960edafc078786622bba35840b4b52
Lior12321 <lior.m.masas@gmail.com>
2024-06-09 16:57:25 +0300

    almost done triangle tests

--- src/unittests/geometries/TriangleTests.java
+++ src/unittests/geometries/TriangleTests.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 
 import geometries.Triangle;
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
 import static primitives.Util.*;
 
+import java.util.List;
 
 public class TriangleTests {
 	/**
@@ -35,6 +37,43 @@ public class TriangleTests {
 				"ERROR: the normal is not orthogonal to the 2nd edge");
 		assertTrue(isZero(t.getNormal(new Point(0, 0, 1)).dotProduct(new Vector(-1, 0, 1))),
 				"ERROR: the normal is not orthogonal to the 3rd edge");
+	}
+
+	/**
+	 * Test method for
+	 * {@link geometries.Triangle#findIntersections(primitives.Ray)}.
+	 */
+	@Test
+	public void testFindIntersections() {
+		Triangle t = new Triangle(new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1));
+		Point p111 = new Point(1, 1, 1);
+		Vector v111 = new Vector(1, 1, 1);
+		// ============ Equivalence Partitions Tests ==============
+		// TC01: Ray intersects the triangle
+		final var result = t.findIntersections(new Ray(p111, new Vector(-1, -1, -0.5)));
+		assertEquals(1, result.size(), "ERROR: Wrong number of points");
+		assertEquals(List.of(new Point(0.2, 0.2, 0.6)), result, "ERROR: Wrong intersection points");
+
+		// TC02: Ray outside against edge
+		assertNull(t.findIntersections(new Ray(new Point(1.1, -0.1, 0), v111)),
+				"ERROR: Wrong intersection points (didn't return null)");
+
+		// TC03: Ray outside against vertex
+		assertNull(t.findIntersections(new Ray(new Point(0.5, 0.5, 1), new Vector(1, -0.5, -1))),
+				"ERROR: Wrong intersection points (didn't return null)");
+
+		// =============== Boundary Values Tests ==================
+		// TC10: Ray on edge
+		assertNull(t.findIntersections(new Ray(new Point(0.5, 0.5, 1), new Vector(-0.5, -0.1, -0.4))),
+				"ERROR: findIntersections() did not return null");
+
+		// TC11: Ray on vertex
+		assertNull(t.findIntersections(new Ray(new Point(0.5, 0.5, 1), new Vector(-0.5, 0.5, -1))),
+				"ERROR: findIntersections() did not return null");
+
+		// TC12: Ray on edge continue
+		assertNull(t.findIntersections(new Ray(new Point(0.5, 0.5, 1), new Vector(-0.5, -1, 0.5))),
+				"ERROR: findIntersections() did not return null");
 	}
 
 }
