eb697c5267bdeea28ce82366ea987c8aa525d0e6
asafyhv <66057736+asafyhv@users.noreply.github.com>
2024-06-09 18:49:51 +0300

    sphere findIntersections

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -31,14 +31,64 @@ public class Sphere extends RadialGeometry {
 		this.center = center;
 	}
 
+	/**
+	 * checks the normal of the sphere
+	 * @param point the point we are calculating the normal from
+	 * @return the normal
+	 */
 	@Override
 	public Vector getNormal(Point point) {
 		return (point.subtract(center)).normalize();
 	}
 
+	/**
+	 * checks for intersections between a given ray and the sphere
+	 * @param ray a given ray
+	 * @return a list of the points of the intersections
+	 */
 	@Override
 	public List<Point> findIntersections(Ray ray) {
-		// TODO Auto-generated method stub
+		// Check if the ray's origin intersects with the center of the sphere
+		if (ray.geHead().equals(center)) {
+			// If so, return the center plus the radius as the only intersection
+			List<Point> intersections = new java.util.ArrayList<Point>();
+			intersections.add(center.add(ray.getDir().scale(radius)));
+			return intersections;
+		}
+		// Calculate the vector from the ray's head to the sphere's center
+		Vector pointToCenter = center.subtract(ray.getHead());
+		// Calculate the projection of this vector onto the ray's direction
+		double tm = pointToCenter.dotProduct(ray.getDir());
+		// Calculate the distance from the sphere's center to the ray
+		double distanceFromCenter = sqrt(pointToCenter.dotProduct(pointToCenter) - tm * tm);
+
+		// If the distance from the center is greater than the radius, there are no intersections
+		if (distanceFromCenter >= radius) {
+			return null;
+		}
+
+		// Calculate the distance along the ray to the intersection points
+		double th = sqrt(radius * radius - distanceFromCenter * distanceFromCenter);
+		double firstDistance = tm - th;
+		double secondDistance = tm + th;
+
+		// If either intersection point is in front of the ray's origin, return them
+		if (firstDistance > 0 || secondDistance > 0) {
+			List<Point> intersections = new java.util.ArrayList<Point>();
+			if (Util.alignZero(firstDistance) > 0) {
+				Point firstIntersection = ray.getPoint(firstDistance);
+				intersections.add(firstIntersection);
+			}
+			if (Util.alignZero(secondDistance) > 0) {
+				Point secondIntersection = ray.getPoint(secondDistance);
+				intersections.add(secondIntersection);
+			}
+			else {
+				return null;
+			}
+			return intersections;
+		}
+		// If no intersection points are found, return null
 		return null;
 	}
 }
